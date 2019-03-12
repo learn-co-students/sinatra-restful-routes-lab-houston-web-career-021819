17222957a2417f0eec2e55887b5ef661ed113770
moranja <jeffadammoran@gmail.com>
2019-03-12 10:58:46 -0500

    done

---
+++ db/migrate/20190312150335_create_recipes.rb
@@ -0,0 +1,9 @@
+class CreateRecipes < ActiveRecord::Migration
+  def change
+    create_table :recipes do |table|
+      table.string :name
+      table.string :ingredients
+      table.integer :cook_time
+    end
+  end
+end
