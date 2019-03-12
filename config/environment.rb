b1c04ba91a770f5c64494b7448a4aa99ac268e47
github username <github email address>
2019-03-12 12:08:32 -0500

    not yet finished/

--- config/environment.rb
+++ config/environment.rb
@@ -7,5 +7,8 @@ ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
+configure :development do
+    set :database, 'sqlite3:db/database.db'
+end
 
 require_all 'app'
